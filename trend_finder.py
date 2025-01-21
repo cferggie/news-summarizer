516db3dfc68128031baa79e9a183ca51d85a0ca5
Chris <chrisferg45@outlook.com>
2025-01-21 16:11:42 -0500

    Refactor CNN article finder: rename class, improve methods for topic navigation, HTML content retrieval, and hyperlink extraction. Update error handling and return types to use dictionaries for better data structure. Adjust main function to reflect changes in class instantiation and method calls.

--- trend_finder.py
+++ trend_finder.py
@@ -3,29 +3,30 @@ from base_scraper import BaseScraper
 
 # For now this will only include CNN articles. Expand once article finder, scraper, and text processing pipeline is finished. 
 # Realized that is probably easier to create an article finder for each possible website.
-class ArticleFinder(BaseScraper):
+class CNNArticleFinder():
     """
     The purpose of this class is to extract the urls for the trending articles of the topic of interest. 
-    The class should return a list of url for the article data extraction tools.
+    The class should return a list of urls for the article data extraction tools.
     """
-
-    def __init__(self, url, user_data: dict):
+    def __init__(self, url, user_data):
         """
-        Initialize the ArticleFinder
+        Initialize the CNNArticleFinder
 
         Args:
-            topics (str): A JSON string containing topics to find articles for.
+            topics (JSON String): A JSON string containing topics to find articles for.
                           Example: '{"topics": ["Technology", "Health"]}'
         """        
         self.topics = json.loads(user_data)['topics']
-        super().__init__(url)
+        self.topic_pages = self.topic_navigation()
+        self.url = url
 
     def topic_navigation(self):
         """
-        Navigates the ArticleFinder to the proper url based on topics selected.
+        Navigates the CNNArticleFinder to the proper url based on topics selected.
 
         Return:
             topic_pages (str): list of strings containing the urls needed for extraction
+                            Example: ['https://cnn.com/politics', 'https://www.cnn.com/world']
         """
         topic_pages = {
             'US': 'https://www.cnn.com/us',
@@ -45,49 +46,51 @@ class ArticleFinder(BaseScraper):
 
         # Get the keys based on user defined topic preference
         try:
-            keys = set(user_topic_pref) & set(topic_pages.keys())
-            pages = [topic_pages[key] for key in keys]
+            keys = set(user_topic_pref) & set(topic_pages.keys())  
+            pages = [topic_pages[key] for key in keys]    
             return pages
         except Exception as e:
             print(f'Error occured in topic navigation: {e}')
 
-    def get_topic_html_content(self):
+    def get_page_soup(self):
         """
         Return
-           html_content (str): list containing the html content to the users desired topics 
+           content (dict): dictionary containing the html content soup objects to the users desired topic pages
         """
-        # Retreive html content
-        pages = self.topic_navigation()
-
-        content = []
-        for page in pages:
-            self.url = page
-            html_content = self.fetch_html_content()
-            content.append(html_content)
-        print(content)
+        # Get the html content for each url
+        content = {}
+        try:
+            for topic in self.topics:
+                for page in self.topic_pages:
+                    base_scraper = BaseScraper(url=page)
+                    page_soup = base_scraper.get_soup()
+                    content[topic] = page_soup
+            return content
+        except Exception as e:
+            print(f'Error occured in get_page_soup: {e}')
 
     def hyperlink_search(self):
         """
         Searches soup for hyperlinks. 
 
         Return:
-            hyperlinks (str): A list containing article hyperlinks in string format
-            
+            hyperlinks (dict): A dictionary containing article hyperlinks in string format     
         """
+        # retrieve page soup
+        soup = self.get_page_soup()
 
-        # retrieve html content
-        soup = self.get_topic_html_content
-
-        # if html content is None
+        # if page soup is None
         if not soup:
-            return None
+            raise ValueError('Page soup could not be found')
         
         try:
-            div = soup.find('div', class_='container_lead-plus-headlines__cards-wrapper')
-            a_elements = div.find_all('a', href=True)
-            href_values = [tag['href'] for tag in a_elements]
-            
-            return href_values
+            content = {}
+            for topic, page in soup.items():
+                div = page.find('div', class_='container_lead-plus-headlines__cards-wrapper')
+                a_elements = div.find_all('a', href=True)
+                href_values = [tag['href'] for tag in a_elements] 
+                content[topic] = href_values
+            return content
         except Exception as e:
             print(f'There was an error with element finding for hyperlink search: {e}')
 
@@ -96,7 +99,7 @@ class ArticleFinder(BaseScraper):
         Uses extracted hyperlinks and converts them to accessable urls for article data extraction tool.
 
         Returns:
-            urls (str): list of urls to be sent to the article data extraction tool
+            urls (dict): dictionary of urls to be sent to the article data extraction tool
         """
         hyperlinks = self.hyperlink_search()
 
@@ -104,12 +107,13 @@ class ArticleFinder(BaseScraper):
             raise ValueError('Hyperlinks for article data extraction tool could not be found')
         
         try:
-            urls = []
-            for hyperlink in hyperlinks:
-                url_prefix = 'cnn.com' 
-                proper_link = url_prefix + hyperlink
-                urls.append(proper_link)
-            
+            urls = {}
+            for topic, hyperlink_list in hyperlinks.items():
+                urls[topic] = []        
+                for hyperlink in hyperlink_list:
+                    url_prefix = 'https://www.cnn.com' 
+                    proper_link = url_prefix + hyperlink
+                    urls[topic].append(proper_link)         
             return urls
         except Exception as e:
             print(f'Error occured in the url creation process: {e}')
@@ -119,12 +123,13 @@ def main():
         'topics': ['Politics', 'World']
     }
     user_data = json.dumps(user_data)
-    article_finder = ArticleFinder(url='https://www.cnn.com/politics', user_data=user_data)
-    print(article_finder.topic_navigation())
-    print(article_finder.get_topic_html_content())
-    # urls = article_finder.get_link()
-    # for url in urls:
-    #     print(url) 
+    article_finder = CNNArticleFinder(
+        url='https://www.cnn.com/politics', 
+        user_data=user_data)
+    print(article_finder.topics)
+    print(article_finder.topic_pages)
+    print(article_finder.hyperlink_search())
+    print(article_finder.get_link())
 
 if __name__ == '__main__':
     main()
